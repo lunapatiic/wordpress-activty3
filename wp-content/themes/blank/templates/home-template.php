2ea1f3f1f074ef454dcba6395a81dfa85eb05973
giziko97 <gheve.mombay97@gmail.com>
2021-05-14 00:55:32 +0800

    midterm backup

--- wp-content/themes/blank/templates/home-template.php
+++ wp-content/themes/blank/templates/home-template.php
@@ -32,36 +32,32 @@ get_header();
 <div class = "container">
 	<div class = "column nine">
 		<div class="card cool">
-  			<a href="#"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/24288596-red-rose-flower-bouquet-isolated-on-white-background-cutout.jpg" style="width:80%"></a>
+  			<a href="http://localhost/wordpress1/product/red-rose-flower/"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/24288596-red-rose-flower-bouquet-isolated-on-white-background-cutout.jpg" style="width:80%"></a>
   			<h1>Roses</h1>
-  			<p class="price">₱150.00 per piece</p>
   			<p>₱700.00 per bundle</p>
   			<p>A cute roses to be given to your lover or a person who you have an interest in.</p>
   		</div>
   	</div>
   	<div class = "column nine">
   		<div class="card cool">
-  			<a href="#"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/download-1.jpg" style="width:80%"></a>
+  			<a href="http://localhost/wordpress1/product/white-lily-flower/"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/download-1.jpg" style="width:80%"></a>
   			<h1>Lily Flower</h1>
-  			<p class="price">₱120.00 per piece</p>
   			<p>₱500 per bundle</p>
   			<p>A cute white lily flower. Pure and pretty</p>
 		</div>
 	</div>
   	<div class = "column nine">
   		<div class="card cool">
-  			<a href="#"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/pink-tulips-flowers.-1.jpg" style="width:80%"></a>
+  			<a href="http://localhost/wordpress1/product/pink-tulip-flower/"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/pink-tulips-flowers.-1.jpg" style="width:80%"></a>
   			<h1>Tulip</h1>
-  			<p class="price">₱150.00 per piece</p>
-  			<p>₱600.00 per bundle</p>
+  			<p>₱590.00 per bundle</p>
   			<p>A cute pink tulip good for giving it to someone you find attractive.</p>
 		</div>
 	</div>
 	  	<div class = "column nine">
   		<div class="card cool">
-  			<a href="#"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/purple-orchid3.png" style="width:80%"></a>
+  			<a href="http://localhost/wordpress1/product/purple-orchid-flower/"><img src="http://localhost/wordpress1/wp-content/uploads/2021/05/purple-orchid3.png" style="width:80%"></a>
   			<h1>Orchids</h1>
-  			<p class="price">₱140.00 per piece</p>
   			<p>₱700.00 per bundle</p>
   			<p>A cute hatsune figurine</p>
 		</div>
